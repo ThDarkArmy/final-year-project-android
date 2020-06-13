6dd457d40a5b49833ff9f94bd80f77d6a1315b66
SK078475 <SK078475@cerner.net>
2020-06-13 16:33:45 +0530

    made holiday updates

--- app/src/main/java/com/tda/finalyear/adapter/HolidayAdapter.java
+++ app/src/main/java/com/tda/finalyear/adapter/HolidayAdapter.java
@@ -1,17 +1,29 @@
 package com.tda.finalyear.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tda.finalyear.R;
+import com.tda.finalyear.activities.holiday.EditHolidayActivity;
+import com.tda.finalyear.activities.holiday.HolidayActivity;
+import com.tda.finalyear.activities.holiday.HolidayListActivity;
+import com.tda.finalyear.api.RetrofitClient;
+import com.tda.finalyear.models.Holiday;
 import com.tda.finalyear.models.HolidayList;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class HolidayAdapter extends RecyclerView.Adapter<HolidayAdapter.HolidayViewHolder> {
@@ -33,9 +45,35 @@ public class HolidayAdapter extends RecyclerView.Adapter<HolidayAdapter.HolidayV
     @Override
     public void onBindViewHolder(@NonNull HolidayViewHolder holder, int position) {
         holder.title.setText(holidays.getHolidays().get(position).getTitle());
-        holder.description.setText(holidays.getHolidays().get(position).getDescription());
-        holder.date.setText(holidays.getHolidays().get(position).getStartDate());
-        holder.duration.setText(holidays.getHolidays().get(position).getDuration().toString());
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(context, EditHolidayActivity.class);
+                i.putExtra("HOLIDAY_ID",holidays.getHolidays().get(position).getId());
+                i.putExtra("HOLIDAY_DESCRIPTION",holidays.getHolidays().get(position).getDescription());
+                i.putExtra("HOLIDAY_DURATION",holidays.getHolidays().get(position).getDuration());
+                i.putExtra("HOLIDAY_TITLE",holidays.getHolidays().get(position).getTitle());
+                i.putExtra("HOLIDAY_START_DATE",holidays.getHolidays().get(position).getStartDate());
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetrofitClient.getInstance().getHolidayService().deleteHoliday(holidays.getHolidays().get(position).getId()).enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if(response.isSuccessful()){
+                            holidays.getHolidays().remove(position);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(context, "Delete Unsuccessful", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -44,13 +82,12 @@ public class HolidayAdapter extends RecyclerView.Adapter<HolidayAdapter.HolidayV
     }
 
     public class HolidayViewHolder extends RecyclerView.ViewHolder {
-        TextView title, duration, description, date;
+        TextView title; Button edit,delete;
         public HolidayViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.card_holiday_title);
-            description = itemView.findViewById(R.id.card_holiday_desc);
-            date = itemView.findViewById(R.id.card_holiday_date);
-            duration = itemView.findViewById(R.id.card_holiday_duration);
+            edit = itemView.findViewById(R.id.editHoliday);
+            delete = itemView.findViewById(R.id.deleteHoliday);
         }
     }
 }
