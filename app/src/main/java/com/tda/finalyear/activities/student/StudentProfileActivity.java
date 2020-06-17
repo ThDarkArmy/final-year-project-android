c25ae8726e6d2289af0938acec064ce066476de2
swabhimankumar <swabhimandarad00@gmail.com>
2020-06-17 14:42:02 +0530

    about school profile student,teacher

---
+++ app/src/main/java/com/tda/finalyear/activities/student/StudentProfileActivity.java
@@ -0,0 +1,16 @@
+package com.tda.finalyear.activities.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tda.finalyear.R;
+
+public class StudentProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+    }
+}
