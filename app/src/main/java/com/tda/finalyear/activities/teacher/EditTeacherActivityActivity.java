dfadc20d484d0a36a7fc182bffafb72b291292b8
swabhimankumar <swabhimandarad00@gmail.com>
2020-06-17 17:28:12 +0530

    profile,editprofile

---
+++ app/src/main/java/com/tda/finalyear/activities/teacher/EditTeacherActivityActivity.java
@@ -0,0 +1,16 @@
+package com.tda.finalyear.activities.teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tda.finalyear.R;
+
+public class EditTeacherActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_teacher_activity);
+    }
+}
